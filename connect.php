d5d65868efc1cd4408ba77592fd2ca7e6ef40f89
Oblivion1212 <60141452+Oblivion1212@users.noreply.github.com>
2020-11-30 18:54:02 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+	$Name = $_POST['Name'];
+	$userName = $_POST['userName'];
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(Name, userName, email, password) values(?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $Name, $userName, $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();  
+		$conn->close();
+	}
+?>
